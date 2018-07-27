6b37a879db350775d140bc75c806e0e06ccb8800
Varshugjsv <41744291+Varshugjsv@users.noreply.github.com>
2018-07-27 18:50:54 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,11 +1,9 @@
 num=int(input())
 count=0
 for i in range(1,num+1):
-    print(i)
     if((num%i==0)):
        count=count+1
        print(count)
-print(count)
 if(count<=2):
     print("yes")
 else:
