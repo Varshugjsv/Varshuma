056dde1d62e1b278078e23b81c2e6f0e51911ab5
Varshugjsv <41744291+Varshugjsv@users.noreply.github.com>
2018-08-06 18:35:17 +0530

    Create armint.py

---
+++ armint.py
@@ -0,0 +1,13 @@
+num=raw_input()
+num=num.split()
+num1=int(num[0])
+num2=int(num[1])
+for i in range(num1,num2+1):
+    temp=i
+    sum=0
+    while(temp!=0):
+        rev=temp%10
+        sum=sum+(rev*rev*rev)
+        temp=temp/10
+    if i==sum:
+        print(i),
