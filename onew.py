b34959b59589d0581347125c0f7cc3396fc3ffb0
Varshugjsv <41744291+Varshugjsv@users.noreply.github.com>
2018-08-10 18:41:31 +0530

    Update onew.py

--- onew.py
+++ onew.py
@@ -1,5 +1,10 @@
-n=raw_input()
-if ord(n)>=48 and ord(n)<=57:
+num=raw_input()
+for i in range(len(num)):
+    if(ord(num[i])>=48 and ord(num[i])<=57 or ord(num[i])==46):
+        flag=0
+    else:
+        flag=1
+if(flag!=1):
     print("Yes")
 else:
     print("No")
